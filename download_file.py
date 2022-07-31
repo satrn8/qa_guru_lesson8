03f5e05441f927803d638a625857c6795db8a931
satrn8 <verypyc@gmail.com>
2022-07-31 17:59:19 +0300

    commit 07/31

---
+++ download_file.py
@@ -0,0 +1,13 @@
+import os
+
+import requests
+from requests import Response
+
+
+r: Response = requests.get("https://selenium.dev/images/selenium_logo_square_green.png")
+
+f = open("selenium.png", "wb")
+f.write(r.content)
+f.close()
+
+print(os.path.getsize("selenium.png"))
---
+++ download_file_from_browser.py
@@ -0,0 +1,24 @@
+import os.path
+import time
+
+from selene.support.shared import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+
+prefs = {
+    "download.default_directory": "..\\qa_guru_lesson8\\tmp",
+    "download.prompt_for_download": False,
+
+}
+options.add_experimental_option("prefs", prefs)
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+browser.config.driver = driver
+browser.open("https://demoqa.com/upload-download")
+browser.element("#downloadButton").click()
+time.sleep(1)
+assert os.path.getsize("..\\qa_guru_lesson8\\tmp\\sampleFile.jpeg") == 4096
+
