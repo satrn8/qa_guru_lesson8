47ded47b0e4573d7978e52ac80d19a99edbfe814
satrn8 <verypyc@gmail.com>
2022-08-07 22:19:19 +0300

    commit 08/07

--- archive_wiht_zip_file.py
+++ archive_wiht_zip_file.py
@@ -3,4 +3,7 @@ from zipfile import ZipFile
 zip_ = ZipFile(".\\resources\\English for Information Technology [Pearson Longman].zip")
 print(zip_.namelist())
 # zip_.extract("English for Information Technology [Pearson Longman].pdf", "tmp")
-zip_.extractall("tmp")
+# zip_.extractall("tmp")
+text = zip_.read("English for Information Technology [Pearson Longman].pdf")
+print(text)
+zip_.close()
